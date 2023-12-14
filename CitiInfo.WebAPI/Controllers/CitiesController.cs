27474fd1bff93215f42ae90ff9211096125ea04a
Huyvictory <vominhhuy008@gmail.com>
2023-12-15 00:06:58 +0700

    Init base API to get list of cities, details of a specific city

---
+++ CitiInfo.WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using CitiInfo.WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiInfo.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCityById(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (cityToReturn == null) { return NotFound(); }
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ CitiInfo.WebAPI/Models/CitiesDataStore.cs
@@ -0,0 +1,19 @@
+﻿namespace CitiInfo.WebAPI.Models
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto() { Id = 1, Name = "New York City", Description = "The one with that big park."},
+                new CityDto() { Id = 2, Name = "Ho Chi Minh", Description = "The one with busy atmosphere"},
+                new CityDto() { Id = 3, Name = "Paris", Description = "The one with that big tower."}
+            };
+        }
+    }
+}
